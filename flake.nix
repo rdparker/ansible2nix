c4a68167ea483e52075d0f1864369f8cf137a0f6
Ron Parker <rdparker@gmail.com>
2022-10-20 18:35:09 -0400

    Add Darwin support

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem ["x86_64-linux"] (system: let
+    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ self.overlay ];
