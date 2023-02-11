d7d31b84ef875cf78326d0f55fd566d96a5ee9bd
Ron Parker <rdparker@gmail.com>
2023-02-11 17:19:17 -0500

    Add Arm support

--- flake.nix
+++ flake.nix
@@ -11,7 +11,12 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachSystem ["x86_64-linux" "x86_64-darwin"] (system: let
+    flake-utils.lib.eachSystem [
+      "aarch64-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ] (system: let
       pkgs = import nixpkgs {
         inherit system;
         overlays = [ self.overlay ];
